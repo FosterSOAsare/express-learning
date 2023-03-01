6803b7b1461970d4b4c97c3459786e4dea70ce6c
fostersoasare <fostersoasare@gmail.com>
2023-03-01 18:59:01 +0000

    Starting over

--- controllers/Launches/Launches.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { controllerGetLaunches, controllerGetALaunch, controllerPostLaunches, controllerDeleteLaunch } = require("../Launches/Launches.controller");
-const launchesRouter = express.Router();
-
-launchesRouter.get("/launches", controllerGetLaunches);
-launchesRouter.get("/launch/:launchId", controllerGetALaunch);
-launchesRouter.post("/launches", controllerPostLaunches);
-launchesRouter.delete("/launches/:launchId", controllerDeleteLaunch);
-module.exports = { launchesRouter };
--- controllers/Planets/Planets.route.js
+++
@@ -1,8 +0,0 @@
-const { controllerGetPlanets, controllerGetAPlanet } = require("./Planets.controller");
-const express = require("express");
-
-const planetsRouter = express.Router();
-planetsRouter.get("/planets", controllerGetPlanets);
-planetsRouter.get("/planet/:planetId", controllerGetAPlanet);
-
-module.exports = { planetsRouter };
